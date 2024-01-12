0942fb41d7d5bfdc321ffea359357ee8f037ad79
OSVALDO ARTURO SERVIN HERNANDEZ <osvaldo.servinh@elektra.com.mx>
2024-01-11 18:01:50 -0600

    Welcome screen in progress

--- app/src/main/java/com/plcoding/calorytracker/ui/theme/Theme.kt
+++ app/src/main/java/com/plcoding/calorytracker/ui/theme/Theme.kt
@@ -7,7 +7,10 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.plcoding.calorytrackerprep.ui.theme.Typography
+import com.plcoding.core_ui.Dimensions
+import com.plcoding.core_ui.LocalSpacing
 
 private val DarkColorPalette = darkColors(
     primary = BrightGreen,
@@ -40,10 +43,15 @@ fun CaloryTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Com
     } else {
         LightColorPalette
     }
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+
+    //de esta forma las dimensions que creamos son accesibles en all the proyecto
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
+
 }
